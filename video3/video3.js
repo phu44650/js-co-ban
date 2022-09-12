b9ee27320668c34fb4bd9c53a0d602b6ddb67116
phu44650 <phu44650@gmail.com>
2022-09-12 17:32:16 +0700

    co ban cac dang du lieu

---
+++ video3/video3.js
@@ -0,0 +1,14 @@
+var a = 5;
+console.log (a)
+var b = 8
+
+console.log('xin chao bai 3:', a+b)
+var c = a+b
+console.log(c)
+var d = `adasdasd''''"""`
+console.log(typeof(d))
+console.log (d)
+// hang so:gia tri khoong doi theo thoi gian
+const x =5;
+let v = 1,n=2,m=3;
+console.log('test string::','v =',v,'n =',n,'m =',m)
