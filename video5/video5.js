a6bcd08395ec7ca08da06d11faf1521951ad9cfa
phu44650 <phu44650@gmail.com>
2022-09-13 13:46:33 +0700

    video5

---
+++ video5/video5.js
@@ -0,0 +1,15 @@
+//string method and propeties-ww3
+console.log('jelo')
+
+var a = `asdsadsadsa`
+console.log(a.length)
+
+//the slice() method- cắt có thể xem trên ư3chool
+var b= `toi laf ai`
+var words = b.split(' ')//trong  ( thứ là dc nagwsn cách bởi)
+console.log(words[2])
+var char = b.split('')
+console.log(char)
+var char = b.split('')
+console.log(char[2])
+
