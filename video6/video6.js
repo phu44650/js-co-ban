be5540d96bf05722c9d48c446b69411cfeedfce7
phu44650 <phu44650@gmail.com>
2022-09-13 14:17:35 +0700

    video6

---
+++ video6/video6.js
@@ -0,0 +1,18 @@
+//number
+console.log(`xin chao mn`.toUpperCase())
+let a = 5;
+let b=6;
+let d=`5`
+console.log(a+b)
+console.log(d*a )
+console.log(a===d,`a:`,typeof a,`d:`,typeof d)
+
+//convent một chuỗi sang số-ở đây convent biến d sang number
+let e  = Number (d);
+console.log('e value :',e,'type:',typeof e)
+
+//cách 2 : nhanh hơn
+
+let q =+d;
+console.log(typeof q)
+console.log(a===q,`a:`,typeof a,`q:`,typeof q)
